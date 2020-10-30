9284a66a5811a607bc70229d85a604be69dda6cf
gSalvatorec <gsalvadorcr0322@gmail.com>
2020-10-29 22:50:43 -0600

    Agregamos logo svg

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ let seccion = document.querySelector('section');
 window.addEventListener('DOMContentLoaded', () => {
     ScrollReveal().reveal('.contenido', {
         scale: 0,
-        origin: 'left',
-        // distance: '40rem',
+        origin: 'rigth',
+        distance: '40rem',
         reset: true,
         delay: 300
     })
